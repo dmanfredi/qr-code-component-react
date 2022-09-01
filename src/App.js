37fe100ac6358a762c490572243a0458b8972837
dman <dylan.k.manfredi@gmail.com>
2022-08-31 19:21:54 -0600

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import qrcode from './images/image-qr-code.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <div className="meta-wrapper">
+        <div className="container">
+          <div className="qr-image-wrapper">
+            <img src={qrcode} alt="QR Code" className="qr-img" />
+          </div>
+          <header className="prompt">
+            Improve your front-end skills by building projects
+          </header>
+          <div className="instructions">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
